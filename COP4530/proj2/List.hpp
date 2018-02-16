a30eb5fb62934e08eb39bf84b5ec679fce0eaed5
Gregory Yentz <gyentz@gmail.com>
2018-02-16 17:26:20 -0500

    update 02/16/18 17:25

--- COP4530/proj2/List.hpp
+++ COP4530/proj2/List.hpp
@@ -1,4 +1,9 @@
-#include"List.h"
+/*
+	Gregory Yentz
+	COP 4530
+	Project 2
+	Doubly-Linked List Container
+*/
 
 //const_interator class//
 template <typename T>
@@ -100,51 +105,151 @@ typename List<T>::iterator List<T>::iterator::operator--(int i){
 }
 
 template <typename T>
-		iterator(Node *p) : const_iterator{p}
-			{}
-};
+List<T>::iterator::iterator(Node *p) : const_iterator{p} {}
 
-List::List(){
+//List Class//
+template <typename T>
+List<T>::List(){
 	init();
 }
-List::List(const List & rhs){
+
+template <typename T>
+List<T>::List(const List & rhs){
 	init();
 	for(auto &x : rhs){
 		push_back(x);
 	}
 }
-List::List(List && rhs)
-	: theSize{rhs.theSize}, head{rhs.head}, tail{rhs.tail}
-{
+template <typename T>
+List<T>::List(List && rhs): theSize{rhs.theSize}, head{rhs.head}, tail{rhs.tail}{
 	rhs.theSize=0;
 	rhs.head = nullptr;
 	rhs.tail = nullptr;	
 }
-//explicit List::List(int num, const T7 val = T{}){
-//
-//}
-//List::List(const_iterator start, const_iterator end){
-//
-//}
-List::~List(){
+
+template <typename T>
+List<T>::List(int num, const T& val){
+	init();
+	int counter = 0;
+	while(counter < num){
+		push_back(val);
+		counter++;
+	}
+}
+
+template <typename T>
+List<T>::List(const_iterator start, const_iterator end){
+	init();
+	for(auto x = start; x != end; x++){
+		push_back(*x);
+	}
+}
+
+template <typename T>
+List<T>::List<std::initializer_list<T> iList>{
+	init();
+	for(auto x = iList.begin(); x !+ iList.end(); x++){
+		push_back(*x);
+	}
+}
+
+template <typename T>
+List<T>::~List(){
 	clear();
 	delete head;
 	delete tail;
 }
-//const List::List& operator=(const List &rhs){
-//
-//}
-const List::List & operator=(const List &rhs){
+
+template <typename T>
+const typename List<T>::List& List<T>::operator=(const List &rhs){
 	List copy = rhs;
 	std::swap(*this, copy);
 	return *this;
 }
-List::List & operator=(List && rhs){
+
+template <typename T>
+typename List<T>::List& List<T>::operator=(List && rhs){
 	std::swap(theSize, rhs.theSize);
 	std::swap(head, rhs.head);
 	std::swap(tail, rhs.tail);
 	return *this;
 }
+
+template <typename T>
+typename List<T>::List& List<T>::List::operator=(std::initializer_list<T> iList){
+	clear();
+	for(auto x = iList.begin(); x != iList.end(); x++){
+		push_back(*x);
+	}
+	return *this;
+}
+
+template <typename T>
+int List<T>::size() const{
+	return theSize;
+}
+
+template <typename T>
+bool List<T>::empty() const{
+	return (theSize==0);
+}
+
+template <typename>
+void List<T>::clear(){
+	while(!empty()){
+		pop_front();
+	}
+}
+
+template <typename T>
+void List<T>::reverse(){
+	Node *n = this->head;
+	while(n != NULL){
+		Node *temp = n->next;
+		n->next = n->prev;
+		n->prev = temp;
+		if(temp == NULL){
+			this->tail = this->head;
+			this->head = n;
+		}
+		n = temp;
+	}
+}
+
+template <typename T>
+T& List<T>::front(){
+	return *begin();
+}
+
+template <typename T>
+const T& List<T>::front() const{
+	return *begin;
+}
+
+template <typename T>
+T& List<T>::back(){
+	return --(*end()):
+}
+
+template <typename T>
+const T& List<T>::back() const{
+	return --(*end());
+}
+
+template <typename T>
+void List<T>::push_front(const T& val){
+	insert(begin(), val);
+}
+
+template <typename T>
+void List<T>::push_front(T&& val){
+	insert(begin(), std::move(val));
+}
+
+template <typename T>
+
+
+
 void List::init(){
 	theSize = 0;
 	head = new Node;
@@ -152,10 +257,6 @@ void List::init(){
 	head->next = tail;
 	tail->prev = head;
 }
-//iterator List::insert(iterator itr, I && 
-
-
-
 
 
 
