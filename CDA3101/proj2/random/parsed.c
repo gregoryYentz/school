f6bf623a350012ff86f6aaafa5cbfb687ca33394
Gregory Yentz <gyentz@gmail.com>
2018-03-19 16:04:15 -0400

    03/19 16:04

--- CDA3101/proj2/random/parsed.c
+++ CDA3101/proj2/random/parsed.c
@@ -15,7 +15,7 @@ struct INSTRUCT{
 };
 
 int PC;
-int dataMem[32];
+int memFile[32];
 int regFile[32];
 
 struct IF_ID{
@@ -52,8 +52,6 @@ struct MEM_WB{
 void printOP(int, int);
 char *decimal_to_binary(int);
 int ipow(int, int);
-
-void printOutput();
 
 int main(){
 	signed int machineInst[100];		//signed integer input
@@ -148,14 +146,8 @@ int main(){
 		printOP(line[i].instOP, line[i].instFunct);
 	}
 
-	for(i=0; i<32; i++){
-		j = 31-i;
-		dataMem[i] = j;
-		regFile[i] = i;
-	}
-		
-	printOutput();
-	printOutput();
+	
+
 
 	return 0;
 }
@@ -235,67 +227,4 @@ int ipow(int base, int exp){
         base *= base;
     }
     return result;
-}
-
-void printOutput(){
-	printf("********************\n");
-	printf("State at the beginning of cycle 0:\n");
-	printf("\tPC = 0\n");
-	printf("\tData Memory:\n");
-	printf("\t\tdataMem[0] = %d\t\tdataMem[16] = %d\n", dataMem[0], dataMem[16]);
-	printf("\t\tdataMem[1] = %d\t\tdataMem[17] = %d\n", dataMem[1], dataMem[17]);
-	printf("\t\tdataMem[2] = %d\t\tdataMem[18] = %d\n", dataMem[2], dataMem[18]);
-	printf("\t\tdataMem[3] = %d\t\tdataMem[19] = %d\n", dataMem[3], dataMem[19]);
-	printf("\t\tdataMem[4] = %d\t\tdataMem[20] = %d\n", dataMem[4], dataMem[20]);
-	printf("\t\tdataMem[5] = %d\t\tdataMem[21] = %d\n", dataMem[5], dataMem[21]);
-	printf("\t\tdataMem[6] = %d\t\tdataMem[22] = %d\n", dataMem[6], dataMem[22]);
-	printf("\t\tdataMem[7] = %d\t\tdataMem[23] = %d\n", dataMem[7], dataMem[23]);
-	printf("\t\tdataMem[8] = %d\t\tdataMem[24] = %d\n", dataMem[8], dataMem[24]);
-	printf("\t\tdataMem[9] = %d\t\tdataMem[25] = %d\n", dataMem[9], dataMem[25]);
-	printf("\t\tdataMem[10] = %d \tdataMem[26] = %d\n", dataMem[10], dataMem[26]);
-	printf("\t\tdataMem[11] = %d \tdataMem[27] = %d\n", dataMem[11], dataMem[27]);
-	printf("\t\tdataMem[12] = %d \tdataMem[28] = %d\n", dataMem[12], dataMem[28]);
-	printf("\t\tdataMem[13] = %d \tdataMem[29] = %d\n", dataMem[13], dataMem[29]);
-	printf("\t\tdataMem[14] = %d \tdataMem[30] = %d\n", dataMem[14], dataMem[30]);
-	printf("\t\tdataMem[15] = %d \tdataMem[31] = %d\n", dataMem[15], dataMem[31]);
-	printf("\tRegisters:\n");
-	printf("\t\tregFile[0] = %d\t\tregFile[16] = %d\n", regFile[0], regFile[16]);	
-	printf("\t\tregFile[1] = %d\t\tregFile[17] = %d\n", regFile[1], regFile[17]);
-	printf("\t\tregFile[2] = %d\t\tregFile[18] = %d\n", regFile[2], regFile[18]);
-	printf("\t\tregFile[3] = %d\t\tregFile[19] = %d\n", regFile[3], regFile[19]);
-	printf("\t\tregFile[4] = %d\t\tregFile[20] = %d\n", regFile[4], regFile[20]);
-	printf("\t\tregFile[5] = %d\t\tregFile[21] = %d\n", regFile[5], regFile[21]);
-	printf("\t\tregFile[6] = %d\t\tregFile[22] = %d\n", regFile[6], regFile[22]);
-	printf("\t\tregFile[7] = %d\t\tregFile[23] = %d\n", regFile[7], regFile[23]);
-	printf("\t\tregFile[8] = %d\t\tregFile[24] = %d\n", regFile[8], regFile[24]);
-	printf("\t\tregFile[9] = %d\t\tregFile[25] = %d\n", regFile[9], regFile[25]);
-	printf("\t\tregFile[10] = %d \tregFile[26] = %d\n", regFile[10], regFile[26]);
-	printf("\t\tregFile[11] = %d \tregFile[27] = %d\n", regFile[11], regFile[27]);
-	printf("\t\tregFile[12] = %d \tregFile[28] = %d\n", regFile[12], regFile[28]);
-	printf("\t\tregFile[13] = %d \tregFile[29] = %d\n", regFile[13], regFile[29]);
-	printf("\t\tregFile[14] = %d \tregFile[30] = %d\n", regFile[14], regFile[30]);
-	printf("\t\tregFile[15] = %d \tregFile[31] = %d\n", regFile[15], regFile[31]);
-	printf("\tIF/ID:\n");
-	printf("\t\tInstruction: NOOP\n");
-	printf("\t\tPCPlus4: 0\n");
-	printf("\tID/EX:\n");
-	printf("\t\tInstruction: NOOP\n");
-	printf("\t\tPCPlus4: 0\n");
-	printf("\t\tbranchTarget: 0\n");
-	printf("\t\treadData1: 0\n");
-	printf("\t\treadData2: 0\n");
-	printf("\t\timmed: 0\n");
-	printf("\t\trs: 0\n");
-	printf("\t\trt: 0\n");
-	printf("\t\trd: 0\n");
-	printf("\tEX/MEM:\n");
-	printf("\t\tInstruction: NOOP\n");
-	printf("\t\taluResult: 0\n");
-	printf("\t\twriteDataReg: 0\n");
-	printf("\t\twriteReg: 0\n");
-	printf("\tMEM/WB:\n");
-	printf("\t\tInstruction: NOOP\n");
-	printf("\t\twriteDataMem: 0\n");
-	printf("\t\twriteDataALU: 0\n");
-	printf("\t\twriteReg: 0\n\n");
 }
